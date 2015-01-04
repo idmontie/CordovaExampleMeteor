dbff39c2f04f1b102268d0bcdc67d461d849443c
Ivan Montiel <idmontie@asu.edu>
2015-01-04 10:39:54 -0700

    inital attempt at raix push

--- cordova-example.js
+++ cordova-example.js
@@ -43,7 +43,93 @@ if (Meteor.isClient) {
           $( '#photo' ).attr( 'src', data )
         }
       } )
+    },
+    'click #push' : function ( e ) {
+      console.log ( 'test' );
+      Push.send({
+        from: 'Test',
+        title: 'Hello',
+        text: 'World',
+        badge: 12,
+        query: {}
+      });
     }
   });
 }
 
+Push.debug = true;
+
+if ( Meteor.isServer ) {
+  // cert/key placed in the private folder
+  var apnsProdCert = Assets.getText('DevChatCert.pem');
+  var apnsProdKey = Assets.getText('DevChatKey.pem');
+
+  var optionsProduction = {
+    apn: {    
+      'passphrase': '',
+      'certData': apnsProdCert,
+      'keyData': apnsProdKey
+    },
+    "production": false,
+    "badge": true,
+    "sound": true,
+    "alert": true,
+    "vibrate": true
+  };
+
+  Push.init( optionsProduction );
+}
+
+if ( Meteor.isClient ) {
+
+  Push.init({
+        apn: {
+            pushId: 'com.mantarayar.cordova',
+            webServiceUrl: 'http://192.168.0.10:3000'
+        },
+        bagde: true,
+        sound: true,
+        alert: true
+    });
+  // Internal events
+  Push.addListener('token', function(token) {
+      // Token is { apn: 'xxxx' } or { gcm: 'xxxx' }
+      alert( JSON.stringify( token ) )
+  });
+
+  Push.addListener('error', function(err) {
+      if (error.type == 'apn.cordova') {
+          console.log(err.error);
+      }
+  });
+
+  Push.addListener('register', function(evt) {
+      // Platform specific event - not really used
+      alert( JSON.stringify( evt ) )
+  });
+
+  Push.addListener('alert', function(notification) {
+      // Called when message got a message in forground
+      alert( JSON.stringify( notification ) )
+  });
+
+  Push.addListener('sound', function(notification) {
+      // Called when message got a sound
+      alert( JSON.stringify( notification ) )
+  });
+
+  Push.addListener('badge', function(notification) {
+      // Called when message got a badge
+      alert( JSON.stringify( notification ) )
+  });
+
+  Push.addListener('startup', function(notification) {
+      // Called when message recieved on startup (cold+warm)
+      alert( JSON.stringify( notification ) )
+  });
+
+  Push.addListener('message', function(notification) {
+      // Called on every message
+      alert( JSON.stringify( notification ) )
+  });
+}
